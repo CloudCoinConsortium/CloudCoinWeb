3d0fdd50d6eb6a8b085680fe4441607c892dca81
SergiyChernyshov <sergmister77@hotmail.com>
2017-06-01 16:30:58 -0700

    Add files via upload
    wip fileutils

---
+++ core/FileUtils.js
@@ -0,0 +1,19 @@
+class FileUtils 
+{
+
+    constructor(rootFolder, importFolder, importedFolder, trashFolder, suspectFolder, frackedFolder,
+    bankFolder, templateFolder, counterfeitFolder, directoryFolder, exportFolder){
+        this.rootFolder = rootFolder;
+            this.importFolder = importFolder;
+            this.importedFolder = importedFolder;
+            this.trashFolder = trashFolder;
+            this.suspectFolder = suspectFolder;
+            this.frackedFolder = frackedFolder;
+            this.bankFolder = bankFolder;
+            this.templateFolder = templateFolder;
+            this.counterfeitFolder = counterfeitFolder;
+            this.directoryFolder = directoryFolder;
+            this.exportFolder = exportFolder;
+    }
+    
+}
