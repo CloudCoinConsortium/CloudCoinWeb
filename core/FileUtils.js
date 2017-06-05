c96ad1e024f3b045b180e8c14ade1530b05b26ca
SergiyChernyshov <sergmister77@hotmail.com>
2017-06-05 16:50:29 -0700

    jpeg wip

--- core/FileUtils.js
+++ core/FileUtils.js
@@ -72,5 +72,63 @@ class FileUtils
     }
 
     //importJSON not neccessary for javascript
+
+    writeJpeg(cc, tag)
+    {
+        let fileSaveSuccessfully = true;
+        //build the cloudcoin string
+        let cloudCoinStr = "01C34A46494600010101006000601D05"; //THUMBNAIL HEADER BYTES
+        for(let i = 0; i < 25; i++) {
+            cloudCoinStr = cloudCoinStr + cc.ans[i];
+        }
+
+        cloudCoinStr += "204f42455920474f4420262044454645415420545952414e545320";
+        cloudCoinStr += "00";// HC: Has comments. 00 = No
+        cloudCoinStr += "00"; // LHC = 100%
+        cc.calcExpirationDate();
+        cloudCoinStr += cc.edHex;
+        cloudCoinStr += "01"; //cc.nn// network number
+        let hexSN = cc.sn.toString(16);
+        let fullHexSN = "";
+        switch (hexSN.Length)
+        {
+                case 1:fullHexSN = ("00000" + hexSN);   break;
+                case 2:fullHexSN = ("0000" + hexSN);    break;
+                case 3: fullHexSN = ("000" + hexSN);    break;
+                case 4:fullHexSN = ("00" + hexSN);      break;
+                case 5: fullHexSN = ("0" + hexSN);   break;
+                case 6:fullHexSN = hexSN;     break;
+        }
+        cloudCoinStr = (cloudCoinStr + fullHexSN);
+        /* BYTES THAT WILL GO FROM 04 to 454 (Inclusive)*/
+        let ccArray = this.hexStringToByteArray(cloudCoinStr);
+
+        /* READ JPEG TEMPLATE*/
+        let jpegBytes = [];
+        switch (cc.getDenomination())
+        {
+            case   1: jpegBytes = readAllBytes(this.templateFolder + "jpeg1.jpg");   break;
+            case   5: jpegBytes = readAllBytes(this.templateFolder + "jpeg5.jpg");   break;
+            case  25: jpegBytes = readAllBytes(this.templateFolder + "jpeg25.jpg");  break;
+            case 100: jpegBytes = readAllBytes(this.templateFolder + "jpeg100.jpg"); break;
+            case 250: jpegBytes = readAllBytes(this.templateFolder + "jpeg250.jpg"); break;
+        }
+
+         /* WRITE THE SERIAL NUMBER ON THE JPEG */
+
+         
+    }
     
+    hexStringToByteArray(HexString)
+    {
+        let NumberChars = HexString.length;
+        let bytes = [];
+        for(let i = 0; i < NumberChars; i += 2)
+        {
+            bytes[i/2] = parseInt(HexString.substring(i, i+2), 16).toString(2);
+            bytes[i/2] = "00000000" + bytes[i/2];
+            bytes[i/2] = bytes[i/2].substring((bytes[i/2].length - 8) ,bytes[i/2].length)
+        }
+        return bytes;
+    }
 }
